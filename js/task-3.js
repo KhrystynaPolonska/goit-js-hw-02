bd707649f6e360d28be8efae4f43de500a4c89d4
kristy <krisposol7@gmail.com>
2025-06-08 13:35:46 +0300

    add two tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,8 @@
 
-Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів spam і sale, і повертає результат перевірки. Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
-
-Доповни код функції таким чином, що:
-Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
-Якщо в рядку відсутні заборонені слова, функція повертає буль false
-
-
 function checkForSpam(message) {
-if 
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
 }
-
-
 
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
